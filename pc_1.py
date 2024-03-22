35c6393076df056a8c70ceb477759dfc23d073f9
EllianRodrigues <104794781+EllianRodrigues@users.noreply.github.com>
2024-03-21 22:10:32 -0300

    pc1 e pc2

--- pc_1.py
+++ pc_1.py
@@ -1,13 +1,28 @@
 from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname
 from threading import Thread
 import rsa 
-
+from rsa import sign, verify
 from cryptography.fernet import Fernet
+import pickle  # Para serialização dos dados
+
+mandar_primeira_mensagen=True
+
+conexao_PC2=False
 
 localhost_ip = gethostbyname('localhost')
 
+ip_PC2 = (localhost_ip, 10113)
+
 ip_PC2_Para_Key = (localhost_ip, 22222)
+
+ip_PC2_Para_Assinatura = (localhost_ip, 22220)
+
+ip_PC2_Para_Verificacao = (localhost_ip, 22000)
+
+
 def receive_messages(socket):
+    global mandar_primeira_mensagen
+    global conexao_PC2
     while True:   
         ciphertext, address = socket.recvfrom(2048)
         if address == ip_PC2_Para_Key:
@@ -15,36 +30,86 @@ def receive_messages(socket):
             key_PC2 = rsa.decrypt(ciphertext, private_key)
             print("key: ", key_PC2)
             cipher_suite_PC2 = Fernet(key_PC2)
-        else:
+      
+        if address == ip_PC2_Para_Assinatura:
+            dados_desserializados = pickle.loads(ciphertext)
+            # Extrair a mensagem e a assinatura
+            sem_assinatura = dados_desserializados['mensagem']
+            assinatura = dados_desserializados['assinatura']
+
+            #pegar chave
+            ac_socket.sendto("Qual_a_chave_publica_PC2".encode(), ac_address)
+            public_key_bytes_PC2, _ = ac_socket.recvfrom(1024)
+            public_key_PC2 = rsa.PublicKey.load_pkcs1(public_key_bytes_PC2)
+            print(f"Chave publica recebida do AC: {public_key_PC2}") 
+
+
+            # Verificar a assinatura com a chave pública
+            if verify(sem_assinatura.encode(), assinatura, public_key_PC2):
+                print("Assinatura válida. Mensagem recebida:", sem_assinatura)
+                conexao_PC2=True
+                mandar_primeira_mensagen=False
+                verification_socket.sendto('Oi, PC2'.encode(),pc2_address)               
+            else:
+                print("Assinatura inválida.")
+                verification_socket.sendto('não_verificado'.encode(),pc2_address) 
+
+      
+        if address == ip_PC2_Para_Verificacao:
+            if ciphertext.decode() == "Oi, PC1":
+                print("PC2 diz: ", ciphertext)
+                mandar_primeira_mensagen=False
+                conexao_PC2=True
+            #else:
+                #print("PC2 diz: ", ciphertext)
+            
+        if address == ip_PC2 and conexao_PC2: 
             print(f"\nMensagem criptografada recebida de {address} mensagem criptografada: {ciphertext}")           
             plaintext = cipher_suite_PC2.decrypt(ciphertext).decode()
             print(f'\nDescriptografado: {plaintext}')
 
-def send_message(socket, address, ac_socket):
-   
+def send_message(socket, address, ac_socket, assing_socket):
+    global mandar_primeira_mensagen
     while True:
         target_pc = input("Insira o número do PC (2 or 6): ")
         if target_pc == '2':
-                                                                 
-            ac_socket.sendto("Qual_a_chave_publica_PC2".encode(), ac_address)
-            public_key_bytes_PC2, _ = ac_socket.recvfrom(1024)
-            public_key_PC2 = rsa.PublicKey.load_pkcs1(public_key_bytes_PC2)
-            print(f"Chave publica recebida do AC: {public_key_PC2}")  
-            
-            key = Fernet.generate_key()
-            print("key: ", key)
-            cipher_key = rsa.encrypt(key, public_key_PC2)
-            print("cypher Key: ", cipher_key)
-            ac_socket.sendto(cipher_key, pc2_address)
-            
-            cipher_suite = Fernet(key)
-                           
-            message = input("Insira a mensagem para PC2: ")
-            message_bytes = cipher_suite.encrypt(message.encode())           
-            socket.sendto(message_bytes, pc2_address)  
-            
-            print(f'mensagem criptografada enviada: {message_bytes}')   
-                  
+                                                                                                    
+            if (mandar_primeira_mensagen):
+                
+                sem_assinatura = input("Insira a primeira mensagem para a conexão: ")
+                assinatura = sign(sem_assinatura.encode(), private_key, 'SHA-256')
+                
+                dados_enviar = {'mensagem': sem_assinatura, 'assinatura': assinatura}
+
+                # Serializar os dados
+                dados_serializados = pickle.dumps(dados_enviar)
+
+                # Enviar os dados serializados
+                assing_socket.sendto(dados_serializados, pc2_address)
+                
+                #mandar_primeira_mensagen=False
+                                            
+            if(mandar_primeira_mensagen==False):    
+                                                                
+                ac_socket.sendto("Qual_a_chave_publica_PC2".encode(), ac_address)
+                public_key_bytes_PC2, _ = ac_socket.recvfrom(1024)
+                public_key_PC2 = rsa.PublicKey.load_pkcs1(public_key_bytes_PC2)
+                print(f"Chave publica recebida do AC: {public_key_PC2}")  
+                
+                key = Fernet.generate_key()
+                print("key: ", key)
+                cipher_key = rsa.encrypt(key, public_key_PC2)
+                print("cypher Key: ", cipher_key)
+                ac_socket.sendto(cipher_key, pc2_address)
+                
+                cipher_suite = Fernet(key)
+                            
+                message = input("Insira a mensagem para PC2: ")
+                message_bytes = cipher_suite.encrypt(message.encode())           
+                socket.sendto(message_bytes, pc2_address)  
+                
+                print(f'mensagem criptografada enviada: {message_bytes}')   
+                                   
         elif target_pc == '6':
             message = input("Insira a mensagem para PC6: ")
             socket.sendto(message.encode(), pc6_address)
@@ -55,9 +120,18 @@ def send_message(socket, address, ac_socket):
 socket_pc1 = socket(AF_INET, SOCK_DGRAM)
 socket_pc1.bind(('localhost', 10112))
 
-# Configuração do socket para comunicação com o AC
+# Configuração do socket para comunicação com o AC  e PC para keys
 ac_socket = socket(AF_INET, SOCK_DGRAM)
 ac_socket.bind(('localhost', 11111))
+
+# configuração para devolver assinado
+assing_socket = socket(AF_INET,SOCK_DGRAM)
+assing_socket.bind(('localhost', 11110))
+
+#configuração para responder a verificação
+verification_socket = socket (AF_INET,SOCK_DGRAM)
+verification_socket.bind(('localhost',11000))
+
 
 # Endereços dos PCs permitidos
 pc2_address = ('localhost', 10113)
@@ -70,11 +144,7 @@ private_key_bytes, _ = socket_pc1.recvfrom(1024)
 private_key = rsa.PrivateKey.load_pkcs1(private_key_bytes)
 print(f"Chave privada recebida do AC: {private_key}")
 
-#socket_pc1.sendto("Qual_a_chave_publica_PC2".encode(), ac_address)
-#public_key_bytes_PC2, _ = socket_pc1.recvfrom(1024)
-#public_key_PC2 = rsa.PublicKey.load_pkcs1(public_key_bytes_PC2)
-#print(f"Chave publica recebida do AC: {public_key_PC2}")
 
 # Threads para receber e enviar mensagens
 Thread(target=receive_messages, args=(socket_pc1,)).start()
-Thread(target=send_message, args=(socket_pc1, pc2_address, ac_socket)).start()
+Thread(target=send_message, args=(socket_pc1, pc2_address, ac_socket, assing_socket)).start()
--- pc_2.py
+++ pc_2.py
@@ -1,49 +1,116 @@
 from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname
 from threading import Thread
 import rsa
-
+from rsa import verify, sign
 from cryptography.fernet import Fernet
+
+import pickle  # Para desserialização dos dados
+
+enivar_primeira_mensagem=True
+
+conexao_pc1=False
 
 localhost_ip = gethostbyname('localhost')
 
+ip_PC1 = (localhost_ip, 10112)
+
 ip_PC1_Para_Key = (localhost_ip, 11111)
 
-def receive_messages(socket):
-    while True:
+ip_PC1_Para_Assinadura = (localhost_ip, 11110)
+
+ip_PC1_Para_Verificacao = (localhost_ip, 11000)
+
+def receive_messages(socket, ac_socket, verification_socket):
+    global enivar_primeira_mensagem
+    global conexao_pc1
+    while True:       
         ciphertext, address = socket.recvfrom(2048)
+             
         if address == ip_PC1_Para_Key:
             print("cipher_key: ", ciphertext.hex())
             key_PC1 = rsa.decrypt(ciphertext, private_key)
             print("key: ", key_PC1)
-            cipher_suite_PC1 = Fernet(key_PC1)
-        else:
+            cipher_suite_PC1 = Fernet(key_PC1)    
+                    
+        if address == ip_PC1_Para_Assinadura:
+            dados_desserializados = pickle.loads(ciphertext)
+            # Extrair a mensagem e a assinatura
+            sem_assinatura = dados_desserializados['mensagem']
+            assinatura = dados_desserializados['assinatura']
+
+            #pegar chave
+            ac_socket.sendto("Qual_a_chave_publica_PC1".encode(), ac_address)
+            public_key_bytes_PC1, _ = ac_socket.recvfrom(1024)
+            public_key_PC1 = rsa.PublicKey.load_pkcs1(public_key_bytes_PC1)
+            print(f"Chave publica recebida do AC: {public_key_PC1}") 
+
+
+            # Verificar a assinatura com a chave pública
+            if verify(sem_assinatura.encode(), assinatura, public_key_PC1):
+                print("Assinatura válida. Mensagem recebida:", sem_assinatura)
+                conexao_pc1=True
+                enivar_primeira_mensagem=False
+                verification_socket.sendto('Oi, PC1'.encode(),pc1_address)               
+            else:
+                print("Assinatura inválida.")
+                verification_socket.sendto('não_verificado'.encode(),pc1_address) 
+                     
+        if address == ip_PC1_Para_Verificacao:
+            if ciphertext.decode() == "Oi, PC2":
+                print("\nPC1 diz: ", ciphertext)
+                enivar_primeira_mensagem=False
+                conexao_pc1=True
+            else:
+                print("\nPC1 diz: ", ciphertext)            
+                                                              
+        if address == ip_PC1 and conexao_pc1:
             print(f"\nMensagem criptografada recebida de {address} mensagem criptografada: {ciphertext}")           
             plaintext = cipher_suite_PC1.decrypt(ciphertext).decode()
             print(f'\nDescriptografado: {plaintext}')
+          
+            
+            
+            
         
 def send_message(socket, address, ac_socket):
     while True:
         target_pc = input("Insira o número do PC (1 or 3): ")
         if target_pc == '1':
             
-            ac_socket.sendto("Qual_a_chave_publica_PC1".encode(), ac_address)
-            public_key_bytes_PC1, _ = ac_socket.recvfrom(1024)
-            public_key_PC1 = rsa.PublicKey.load_pkcs1(public_key_bytes_PC1)
-            print(f"Chave publica recebida do AC: {public_key_PC1}") 
+            if (enivar_primeira_mensagem):
+                
+                sem_assinatura = input("Insira a primeira mensagem para a conexão: ")
+                assinatura = sign(sem_assinatura.encode(), private_key, 'SHA-256')
+                
+                dados_enviar = {'mensagem': sem_assinatura, 'assinatura': assinatura}
+
+                # Serializar os dados
+                dados_serializados = pickle.dumps(dados_enviar)
+
+                # Enviar os dados serializados
+                assing_socket.sendto(dados_serializados, pc1_address)
+                
+                #mandar_primeira_mensagen=False
             
-            key = Fernet.generate_key()
-            print("key: ", key)
-            cipher_key = rsa.encrypt(key, public_key_PC1)
-            print("cypher Key: ", cipher_key)
-            ac_socket.sendto(cipher_key, pc1_address)
-                     
-            cipher_suite = Fernet(key)
-                           
-            message = input("Insira a mensagem para PC2: ")
-            message_bytes = cipher_suite.encrypt(message.encode())           
-            socket.sendto(message_bytes, pc1_address)  
-           
-            print(f'mensagem criptografada enviada: {message_bytes}')  
+            if(enivar_primeira_mensagem==False):
+                ac_socket.sendto("Qual_a_chave_publica_PC1".encode(), ac_address)
+                public_key_bytes_PC1, _ = ac_socket.recvfrom(1024)
+                public_key_PC1 = rsa.PublicKey.load_pkcs1(public_key_bytes_PC1)
+                print(f"Chave publica recebida do AC: {public_key_PC1}") 
+                
+                key = Fernet.generate_key()
+                print("key: ", key)
+                cipher_key = rsa.encrypt(key, public_key_PC1)
+                print("cypher Key: ", cipher_key)
+                ac_socket.sendto(cipher_key, pc1_address)
+                        
+                cipher_suite = Fernet(key)
+                            
+                message = input("Insira a mensagem para PC1: ")
+                message_bytes = cipher_suite.encrypt(message.encode())           
+                socket.sendto(message_bytes, pc1_address)  
+            
+                print(f'mensagem criptografada enviada: {message_bytes}')  
         
         elif target_pc == '3':
             message = input("Insira a mensagem para PC3: ")
@@ -55,9 +122,17 @@ def send_message(socket, address, ac_socket):
 socket_pc2 = socket(AF_INET, SOCK_DGRAM)
 socket_pc2.bind(('localhost', 10113))
 
-# Configuração do socket para comunicação com o AC
+# Configuração do socket para comunicação com o AC e PC para keys
 ac_socket = socket(AF_INET, SOCK_DGRAM)
 ac_socket.bind(('localhost', 22222))
+
+# configuração para devolver assinado
+assing_socket = socket(AF_INET,SOCK_DGRAM)
+assing_socket.bind(('localhost', 22220))
+
+#configuração para responder a verificação
+verification_socket = socket (AF_INET,SOCK_DGRAM)
+verification_socket.bind(('localhost',22000))
 
 # Endereços dos PCs permitidos
 pc1_address = ('localhost', 10112)
@@ -73,5 +148,5 @@ print(f"Chave privada recebida do AC: {private_key}")
 
 
 # Threads para receber e enviar mensagens
-Thread(target=receive_messages, args=(socket_pc2,)).start()
+Thread(target=receive_messages, args=(socket_pc2,ac_socket,verification_socket)).start()
 Thread(target=send_message, args=(socket_pc2, pc3_address, ac_socket)).start()
